a1cac462205b857f08cf23c095baf161647d1309
Thomas Rossow <rosso@parttimegeeks.net>
2014-01-24 12:26:56 +0100

    Cleanup and documentation of Executor.pm

--- scripts/lib/TrustAtHsH-Irondemo/lib/TrustAtHsH/Irondemo/Executor.pm
+++ scripts/lib/TrustAtHsH-Irondemo/lib/TrustAtHsH/Irondemo/Executor.pm
@@ -8,6 +8,11 @@ use Thread::Queue;
 use Carp qw(croak);
 use Scalar::Util qw(blessed);
 
+### CONSTRUCTOR ###
+# Purpose    : Constructor
+# Returns    : Instance
+# Parameters : threadpool_size -> number of threads to spawn (optional)
+# Comments   :
 sub new {
 	my $class = shift;
 	
@@ -19,44 +24,83 @@ sub new {
 	$self->{'threads'}  = $args->{'threadpool_size'} || 10;
 	$self->{'Qwork'}    = new Thread::Queue;
 	$self->{'Qresult'}  = new Thread::Queue;
-	$self->{'pool'}     = [$self->create_thread_pool()];
-	return $self;
+	$self->{'pool'}     = [$self->_create_thread_pool()];
 	
+	return $self;	
 }
 
+
+### INSTANCE METHOD ###
+# Purpose     : Execute a module
+# Returns     : Exit status of module
+# Parameters  : TrustAtHsH::Irondemo::AbstractModule
+# Comments    :
 sub run {
 	my $self = shift;
 	
 	my $job  = shift;
 	
-	$job->execute();
+	return $job->execute();
 }
 
+
+### INSTANCE METHOD ###
+# Purpose     : Execute a number of modules concurrently
+# Returns     : Nothing
+# Parameters  : Array of TrustAtHsH::Irondemo::AbstractModule
+# Comments    :
 sub run_concurrent {
 	my $self = shift;
 	
 	my @jobs = @_;
 	
 	for my $job ( @jobs ) {
-		$self->get_working_queue->enqueue($job);
+		$self->_get_working_queue->enqueue($job);
 	}
 }
 
-sub create_thread_pool {
+
+### INSTANCE METHOD ###
+# Purpose     : Getter for result queue
+# Returns     : Thread::Queue
+# Parameters  : None
+# Comments    :
+sub get_result_queue {
+	my $self = shift;
+	
+	return $self->{'Qresult'};
+}
+
+
+### INTERNAL UTILITY ###
+# Purpose     : Build initial thread pool
+# Returns     : Array of thread objects
+# Parameters  : None
+# Comments    :
+sub _create_thread_pool {
 	my $self = shift;
 	
 	my $threads = $self->{'threads'};
 	my @pool;
 	
 	for (1..$threads) {
-		my $thread = threads->create( \&worker, $self->get_working_queue(), $self->get_result_queue() );
+		my $thread = threads->create(
+			\&_worker, $self->_get_working_queue(),
+			 $self->get_result_queue()
+		);
 		push @pool, $thread;
-	}
-	
+	}	
 	return @pool;
 }
 
-sub worker {
+
+### INTERNAL UTILITY ###
+# Purpose     : Main routine for worker threads
+# Returns     : Thread ID
+# Parameters  : Working Queue(Thread::Queue),
+#               Result Queue (Thread::Queue)
+# Comments    :
+sub _worker {
 	my $tid = threads->tid;
 	my( $Qwork, $Qresults ) = @_;
 
@@ -64,31 +108,42 @@ sub worker {
 		my $class = blessed $job;
 		eval "require $class";
 		my $result = $job->execute();
-		$Qresults->enqueue( {$tid => 'binky'} );
+		$Qresults->enqueue( {$tid => $result} );
 	}
+	return $tid;
 }
 
-sub get_working_queue {
+
+### INTERNAL UTILITY ###
+# Purpose     : (Internal) getter for working queue
+# Returns     : Thread::Queue
+# Parameters  : None
+# Comments    :
+sub _get_working_queue {
 	my $self = shift;
 	
 	return $self->{'Qwork'};
 }
 
-sub get_result_queue {
-	my $self = shift;
-	
-	return $self->{'Qresult'};
-}
 
+### INTERNAL UTILITY ###
+# Purpose     : Terminate threads on object destruction
+# Returns     : Nothing
+# Parameters  : None
+# Comments    : 
 sub DESTROY {
 	my $self = shift;
 
+	#make sure all threads end
 	for my $thread ( @{$self->{'pool'}} ) {
 		$self->get_working_queue()->enqueue(undef);
 	}
+	
+	#wait for all threads to finish
 	for my $thread ( @{$self->{'pool'}} ) {
 		$thread->join;
 	}
 }
+
 
 1;
