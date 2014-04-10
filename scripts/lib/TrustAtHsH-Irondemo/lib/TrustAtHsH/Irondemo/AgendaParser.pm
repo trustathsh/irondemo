08fa1a68ba992b1cab60bc2e88acb2b0e56f0503
Thomas Rossow <rosso@parttimegeeks.net>
2014-04-10 14:01:38 +0200

    Fixes formatting of AgendaParser.pm

--- scripts/lib/TrustAtHsH-Irondemo/lib/TrustAtHsH/Irondemo/AgendaParser.pm
+++ scripts/lib/TrustAtHsH-Irondemo/lib/TrustAtHsH/Irondemo/AgendaParser.pm
@@ -13,20 +13,19 @@ use Log::Log4perl;
 
 my $log = Log::Log4perl->get_logger();
 
-my $AGENDA_TYPE_SEQ = "seq_agenda";
+my $AGENDA_TYPE_SEQ   = "seq_agenda";
 my $AGENDA_TYPE_TIMED = "timed_agenda";
-
 
 sub new {
 	my $class = shift;
-	my $args = shift;
+	my $args  = shift;
 
 	my $self = {};
 	bless $self, $class;
 
-    $self->{'agenda_path'} = $args->{'path'};
+	$self->{'agenda_path'} = $args->{'path'};
 
-    my $grammar = q {
+	my $grammar = q {
         startrule   : token                           { $return = @item[1];                                     }
         token       : comment(s?) action              { $return = $item[2];                                     }
         comment     : /^\s*#.*;?\n/
@@ -41,77 +40,84 @@ sub new {
         moduleName  : /[A-Z]\w*/
         timestamp   : /\d+/
     };
-    $self->{'parser'} = Parse::RecDescent->new($grammar) or die "Bad grammar!\n";
+	$self->{'parser'} = Parse::RecDescent->new($grammar) or die "Bad grammar!\n";
 
-    return $self;
+	return $self;
 }
-
 
 sub get_actions {
 	my $self = shift;
 
-    my @actions;
-    my $filename = $self->{'agenda_path'};
+	my @actions;
+	my $filename = $self->{'agenda_path'};
 
-    # change new line delimiter
-    $/ = ";";
+	# change new line delimiter
+	$/ = ";";
 
-    my $agenda_file = IO::File->new($filename, 'r');
-    if (defined $agenda_file) {
-        my $current_tick = 0;
-        my $agenda_type = undef;
-        while (my $token = $agenda_file->getline()) {
-            # skip comments at file end?
-            last if eof($agenda_file) && ($token =~ /^\s*#.*$/ || $token =~ /\s*/);
+	my $agenda_file = IO::File->new( $filename, 'r' );
+	if ( defined $agenda_file ) {
+		my $current_tick = 0;
+		my $agenda_type  = undef;
+		while ( my $token = $agenda_file->getline() ) {
 
-            my $actionParsed = $self->{'parser'}->startrule($token);
-            if ($actionParsed) {
+			# skip comments at file end?
+			last if eof($agenda_file) && ( $token =~ /^\s*#.*$/ || $token =~ /\s*/ );
 
-                # check the agenda type:
-                if (!$agenda_type) {
-                    if (defined $actionParsed->{'time'}) {
-                        $agenda_type = $AGENDA_TYPE_TIMED;
-                    } else {
-                        $agenda_type = $AGENDA_TYPE_SEQ;
-                    }
-                    $log->debug("detected agenda type '$agenda_type'");
-                } else {
-                    $log->warn("timed action in sequential agenda") if (
-                                defined $actionParsed->{'time'} && $AGENDA_TYPE_SEQ eq $agenda_type);
-                }
-                my $action = {};
-                my $args = {};
+			my $actionParsed = $self->{'parser'}->startrule($token);
+			if ($actionParsed) {
 
-                for my $i (@{$actionParsed->{'action'}->{'args'}}) {
-                    my $key = $i->{'key'};
-                    my $value = $i->{'value'};
-                    $args->{$key} = $value;
-                }
-                $action->{'args'} = $args;
+				# check the agenda type:
+				if ( !$agenda_type ) {
+					if ( defined $actionParsed->{'time'} ) {
+						$agenda_type = $AGENDA_TYPE_TIMED;
+					}
+					else {
+						$agenda_type = $AGENDA_TYPE_SEQ;
+					}
+					$log->debug("detected agenda type '$agenda_type'");
+				}
+				else {
+					$log->warn("timed action in sequential agenda")
+					  if ( defined $actionParsed->{'time'} && $AGENDA_TYPE_SEQ eq $agenda_type );
+				}
+				my $action = {};
+				my $args   = {};
 
-                $action->{'action'} = $actionParsed->{'action'}->{'module'};
-                my $time;
-                if ($agenda_type eq $AGENDA_TYPE_TIMED) {
-                    if (!defined $actionParsed->{'time'}) {
-                        croak("missing timestamp for action '".$actionParsed->{'action'}->{'module'}."'");
-                    }
-                    $time = $actionParsed->{'time'};
-                } else {
-                    $time = $current_tick;
-                    $current_tick += 1;
-                }
-                $action->{'time'} = $time;
-                push(@actions, $action);
+				for my $i ( @{ $actionParsed->{'action'}->{'args'} } ) {
+					my $key   = $i->{'key'};
+					my $value = $i->{'value'};
+					$args->{$key} = $value;
+				}
+				$action->{'args'} = $args;
 
-            } else {
-                croak("could not parse '$token'");
-            }
-        }
-        undef $agenda_file;
-    } else {
-        die("could not open $filename\n");
-    }
-    my @sorted_actions = sort { $a->{'time'} <=> $b->{'time'} } @actions;
+				$action->{'action'} = $actionParsed->{'action'}->{'module'};
+				my $time;
+				if ( $agenda_type eq $AGENDA_TYPE_TIMED ) {
+					if ( !defined $actionParsed->{'time'} ) {
+						croak(  "missing timestamp for action '"
+							  . $actionParsed->{'action'}->{'module'}
+							  . "'" );
+					}
+					$time = $actionParsed->{'time'};
+				}
+				else {
+					$time = $current_tick;
+					$current_tick += 1;
+				}
+				$action->{'time'} = $time;
+				push( @actions, $action );
+
+			}
+			else {
+				croak("could not parse '$token'");
+			}
+		}
+		undef $agenda_file;
+	}
+	else {
+		die("could not open $filename\n");
+	}
+	my @sorted_actions = sort { $a->{'time'} <=> $b->{'time'} } @actions;
 	return @sorted_actions;
 }
 
