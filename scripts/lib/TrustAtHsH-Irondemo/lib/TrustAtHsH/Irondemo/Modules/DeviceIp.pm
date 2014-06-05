2e67f227b06d18e035a35cc774816a9981b8f848
Leonard <leonard.renners@hs-hannover.de>
2014-06-05 16:06:49 +0200

    Added basic modules for device-ip accessrequest-ip and a test agenda for ifmapcli, currently used to reproduce a bug in visitmeta visualization on live perspective

---
+++ scripts/lib/TrustAtHsH-Irondemo/lib/TrustAtHsH/Irondemo/Modules/DeviceIp.pm
@@ -0,0 +1,81 @@
+package TrustAtHsH::Irondemo::Modules::DeviceIp;
+
+use 5.006;
+use strict;
+use warnings;
+use Carp qw(croak);
+use File::Spec;
+use File::Basename;
+use lib '../../../';
+use parent 'TrustAtHsH::Irondemo::AbstractIfmapCliModule';
+
+my $IP         = 'ip-address';
+my $DEVICE     = 'device';
+my $MODE	   = 'mode';
+my $IFMAP_USER = 'ifmap-user';
+my $IFMAP_PASS = 'ifmap-pass';
+
+my @REQUIRED_ARGS = ( $IP, $DEVICE, $MODE );
+
+### INSTANCE METHOD ###
+# Purpose     :
+# Returns     : True value on success, false value on failure
+# Parameters  : None
+# Comments    :
+sub execute {
+	my $self = shift;
+	my $data = $self->{'data'};
+
+	my @argsList = ( $data->{$DEVICE}, $data->{$IP} );
+
+	my $connectionArgs = {
+		"ifmap-user" => $data->{$IFMAP_USER},
+		"ifmap-pass" => $data->{$IFMAP_PASS}
+	};
+
+	return $self->call_ifmap_cli(
+		{
+			'cli_tool'        => "dev-ip",
+			'mode'            => $data->{$MODE},
+			'args_list'       => \@argsList,
+			'connection_args' => $connectionArgs
+		}
+	);
+
+}
+
+### INSTANCE METHOD ###
+# Purpose     :
+# Returns     :
+# Parameters  :
+# Comments    :
+sub get_required_arguments {
+	my $self = shift;
+
+	return @REQUIRED_ARGS;
+}
+
+### INTERNAL UTILITY ###
+# Purpose     :
+# Returns     :
+# Parameters  : data ->
+#                 ifmap-user          ->(optional)
+#                 ifmap-pass          ->(optional)
+#                 ifmap-url           ->(optional)
+#                 ifmap-keystore-path ->(optional)
+#                 ifmap-keystore-pass ->(optional)
+#                 pdp            ->
+#                 access-request ->
+#                 mac            ->
+#                 ip-address     ->
+# Comments    : Override, called from parent's constructor
+sub _init {
+	my $self = shift;
+	my $args = shift;
+
+	while ( my ( $key, $val ) = each %{$args} ) {
+		$self->{'data'}->{$key} = $val;
+	}
+}
+
+1;
