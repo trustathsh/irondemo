4cb8e1e860c2b652316927a513e1c1a14f501c7e
Thomas Rossow <rosso@parttimegeeks.net>
2014-11-11 14:01:26 +0100

    Adds methods to create vulnerability and implementation related identifiers/metadata to SimuUtilities

--- scripts/lib/TrustAtHsH-Irondemo/lib/TrustAtHsH/Irondemo/SimuUtilities.pm
+++ scripts/lib/TrustAtHsH-Irondemo/lib/TrustAtHsH/Irondemo/SimuUtilities.pm
@@ -16,8 +16,46 @@ sub create_string_id_service {
 	my $string = 
 	  "<simu:service type=\"" . $href_args->{'type'} .
 	  "\" name=\"" . $href_args->{'name'} . "\"" . " port=\"" . $href_args->{'port'} .
-	  "\" administrative-domain=\"" . $href_args->{'administrative_domain'} . 
-	  "\" xmlns:simu=\"http://simu-project.de/XMLSchema/1\" />";
+	  "\" administrative-domain=\"" . $href_args->{'administrative_domain'} . "\"" . 
+	  " xmlns:simu=\"http://simu-project.de/XMLSchema/1\" />";
+	
+	return $string;
+}
+
+sub create_string_id_implementation {
+	my $class     = shift;
+	my $href_args = shift;
+	
+	my $string =
+	  "<implementation name=\"" . $href_args->{'name'} . "\"" .
+	  "version=\"" . $href_args->{'version'} . "\"";
+	
+	if ( $href_args->{'local_version'} ) {
+		$string .= " local-version=\"" . $href_args->{'local-version'} . "\"";
+	}
+	
+	if ( $href_args->{'platform'} ) {
+		$string .= " platform=\"" . $href_args->{'platform'} . "\"";
+	}
+	
+	$string .= " xmlns:simu=\"http://simu-project.de/XMLSchema/1\" />";
+	
+	return $string;
+}
+
+sub create_string_id_vulnerability {
+	my $class = shift;
+	my $href_args = shift;
+	
+	my $string = 
+	  "<vulnerability type=\"" . $href_args->{'type'} . "\"" .
+	  " id=\"" . $href_args->{'id'} . "\"";
+	
+	if ( $href_args-> {'severity'} ) {
+		$string .= " <severity=\"" . $href_args-> {'severity'} . "\"";
+	}
+	
+	$string .= " xmlns:simu=\"http://simu-project.de/XMLSchema/1\" />";
 	
 	return $string;
 }
@@ -32,8 +70,20 @@ sub create_string_meta_login_failed {
 	  "<simu:credential-type>" . $href_args->{'credential_type'} . "</simu:credential-type>" . 
 	  "<simu:reason>" . $href_args->{'reason'} . "</simu:reason>" .
 	  "</simu:login-failure>";
-	
+
 	return $string;
+}
+
+sub create_string_meta_service_implementation {
+	my $class = shift;
+	
+	return "<simu:service-implementation xmlns:simu=\"http://simu-project.de/XMLSchema/1\" ifmap-cardinality=\"singleValue\" />";
+}
+
+sub create_string_meta_implementation_vulnerability {
+	my $class = shift;
+	
+	return "<simu:implementation-vulnerability xmlns:simu=\"http://simu-project.de/XMLSchema/1\" ifmap-cardinality=\"singleValue\" />";
 }
 
 1;
