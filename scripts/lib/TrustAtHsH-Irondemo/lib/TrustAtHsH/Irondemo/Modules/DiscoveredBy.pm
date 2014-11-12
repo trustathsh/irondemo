94be1b9f7e6a5e469393633afbf0c7c172e2ab94
Leonard Renners <leonard.renners@hs-hannover.de>
2014-11-12 14:13:44 +0100

    added missing resources - oops, sorry!

---
+++ scripts/lib/TrustAtHsH-Irondemo/lib/TrustAtHsH/Irondemo/Modules/DiscoveredBy.pm
@@ -0,0 +1,81 @@
+package TrustAtHsH::Irondemo::Modules::DiscoveredBy;
+
+use 5.006;
+use strict;
+use warnings;
+use Carp qw(croak);
+use File::Spec;
+use File::Basename;
+use lib '../../../';
+use parent 'TrustAtHsH::Irondemo::AbstractIfmapCliModule';
+
+my $DEVICE     = 'device';
+my $IDTYPE     = 'id-type';
+my $IDENTIFIER = 'identifier';
+my $IFMAP_USER = 'ifmap-user';
+my $IFMAP_PASS = 'ifmap-pass';
+
+my @REQUIRED_ARGS = ( $DEVICE, $IDTYPE, $IDENTIFIER );
+
+### INSTANCE METHOD ###
+# Purpose     :
+# Returns     : True value on success, false value on failure
+# Parameters  : None
+# Comments    :
+sub execute {
+	my $self = shift;
+	my $data = $self->{'data'};
+
+	my @argsList = ( $data->{$IDTYPE}, $data->{$IDENTIFIER}, $data->{$DEVICE} );
+
+	my $connectionArgs = {
+		"ifmap-user" => $data->{$IFMAP_USER},
+		"ifmap-pass" => $data->{$IFMAP_PASS}
+	};
+
+	return $self->call_ifmap_cli(
+		{
+			'cli_tool'        => "disc-by",
+			'mode'            => "update",
+			'args_list'       => \@argsList,
+			'connection_args' => $connectionArgs
+		}
+	);
+
+}
+
+### INSTANCE METHOD ###
+# Purpose     :
+# Returns     :
+# Parameters  :
+# Comments    :
+sub get_required_arguments {
+	my $self = shift;
+
+	return @REQUIRED_ARGS;
+}
+
+### INTERNAL UTILITY ###
+# Purpose     :
+# Returns     :
+# Parameters  : data ->
+#                 ifmap-user          ->(optional)
+#                 ifmap-pass          ->(optional)
+#                 ifmap-url           ->(optional)
+#                 ifmap-keystore-path ->(optional)
+#                 ifmap-keystore-pass ->(optional)
+#                 pdp            ->
+#                 access-request ->
+#                 mac            ->
+#                 ip-address     ->
+# Comments    : Override, called from parent's constructor
+sub _init {
+	my $self = shift;
+	my $args = shift;
+
+	while ( my ( $key, $val ) = each %{$args} ) {
+		$self->{'data'}->{$key} = $val;
+	}
+}
+
+1;
